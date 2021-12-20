83ab0eeff6c6fbbb2d4d95c5f1b1f6f880608a6f
Enrique Hidalgo <koko.freelance@gmail.com>
2021-12-20 16:45:56 +0000

    Form created

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,13 +1,40 @@
-import react from "react"
-import { Card, Form, Button } from "bootstrap"
+import React, { useRef } from 'react'
+import { Card, Form, Button } from "react-bootstrap"
 
-import React from 'react'
+
 
 function Signup() {
-  return (
-    <div>
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
 
-    </div>
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2>Sign Up</h2>
+          <Form>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
+            <Button type="submit" >Sign Up</Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div>
+        Already have an account? Log In
+      </div>
+    </>
   )
 }
 
