0ce2f09b5842c88d5872b0231fbc08c9b7e61ab1
Enrique Hidalgo <koko.freelance@gmail.com>
2021-12-20 16:34:00 +0000

    firebase set up

---
+++ src/components/Signup.js
@@ -0,0 +1,14 @@
+import react from "react"
+import { Card, Form, Button } from "bootstrap"
+
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Signup
